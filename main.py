bb39d201c69d975ceff10f3bbc21d45d13d08d84
Barışcan Çeken <144244486+bariscanceken@users.noreply.github.com>
2025-04-04 20:26:08 +0300

    asteroids and movies.

--- main.py
+++ main.py
@@ -5,12 +5,86 @@ import os
 import tkinter
 import numpy as np
 import pandas as pd
+from selenium import webdriver
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import base64
+import os
+
+#OS
+base_dir = os.path.dirname(os.path.abspath(__file__))
+path_file = os.path.join(base_dir,'spacemoives.pdf')
 
 #API
 apikey = "aOe1emfgmlMK8SebzLaVeQcNjHX3LuOYHuoptRLC"
 
 #try
 base_url = f"https://api.nasa.gov/planetary/apod?api_key={apikey}"
-response = requests.get(base_url)
-print(response.text)
+base_url2 = f"https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key={apikey}"
+response = requests.get(base_url2)
+data = response.json()
 
+#Asteroids
+asteroids = []
+
+for date in data['near_earth_objects']:
+    for asteroid in data['near_earth_objects'][date]:
+        asteroids.append({
+            'name': asteroid['name'],
+            'id': asteroid['id'],
+            'approach_date': date,
+            'velocity_km_h': float(asteroid['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']),
+            'miss_distance_km': float(asteroid['close_approach_data'][0]['miss_distance']['kilometers']),
+            'diameter_min': asteroid['estimated_diameter']['meters']['estimated_diameter_min'],
+            'diameter_max': asteroid['estimated_diameter']['meters']['estimated_diameter_max'],
+            'is_hazardous': asteroid['is_potentially_hazardous_asteroid']
+        })
+
+df = pd.DataFrame(asteroids)
+print(df)
+
+#Selenium
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--window-size=1920,3000')
+
+driver = webdriver.Chrome(options=options)
+driver.get("https://letterboxd.com/tarokhnstuff/list/space/by/rating/")
+time.sleep(2)
+
+scroll_pause = 0.5
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(scroll_pause)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+time.sleep(2)
+
+#pdf save
+pdf = driver.execute_cdp_cmd("Page.printToPDF", {
+    "printBackground": True,
+    "landscape": False,
+    "paperWidth": 8.27,
+    "paperHeight": 11.69,
+    "marginTop": 0,
+    "marginBottom": 0,
+    "marginLeft": 0,
+    "marginRight": 0,
+})
+
+with open(path_file, "wb") as f:
+    f.write(base64.b64decode(pdf['data']))
+
+driver.quit()
